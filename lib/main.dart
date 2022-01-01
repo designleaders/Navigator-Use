b9398f35cbae74d634092c3abe2b05992346affe
amitgain1818 <amitgain1818@gmail.com>
2022-01-01 17:01:24 +0600

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,21 @@
+import 'package:demo/views/home.dart';
 import 'package:flutter/material.dart';
-import 'SecondRoute.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Amit',
-    home: Main(),
-  ));
-}
+class Recipe extends StatelessWidget {
+  const Recipe({Key? key}) : super(key: key);
 
-class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black87,
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        title: Center(
-          child: Text('Main Page'),
+    return MaterialApp(
+        title: 'Restaurnat Recipe',
+        theme: ThemeData(
+          primaryColor: Colors.orange,
+          primarySwatch: Colors.red,
+          textTheme: TextTheme(
+            bodyText2: TextStyle(fontSize: 24),
+          ),
         ),
-      ),
-      body: Center(
-        child: RaisedButton(
-          color: Colors.teal,
-          textColor: Colors.white,
-          hoverColor: Colors.pink,
-          child: Text('Open Page'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondPage()),
-            );
-          },
-        ),
-      ),
-    );
+        home: HomePage(),
+        );
   }
 }
---
+++ lib/views/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './widgets/recipe_card.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Restaurant Menu'),
+          ],
+        ),
+      ),
+      body: RecipeCard(
+        title: 'My recipe',
+        rating: '4.9',
+        cookTime: '30 min',
+        thumbnailUrl:
+            'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+      ),
+    );
+  }
+}
